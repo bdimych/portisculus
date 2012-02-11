ae18753f8ddf9f48fe35a71035c5a9217b62d941
bdimych <bdimych@faf0b38c-ac3b-0ead-faf0-b6b3d23b80b6>
2012-02-11 12:43:08 +0000

    determining bpm 75% done

--- trunk/count-bpm-by-hands.sh
+++ trunk/count-bpm-by-hands.sh
@@ -1,37 +1,103 @@
 #!/bin/bash
 
-trap 'stty echo' EXIT
-echo space - count, enter - counting done, r - restart counting, p - pause, left/right - seek -/+10 seconds >&2
-while IFS='' read -n1 -s k
-do
-	if [[ $k == ' ' ]]
-	then
-		echo space >&2
-	elif [[ ! $k ]]
-	then
-		echo quit
-		break
-	elif [[ $k == r ]]
-	then
-		echo restart >&2
-	elif [[ $k == p ]]
-	then
-		echo pause
-		echo pause >&2
-	elif [[ $k == $'\e' ]]
-	then
-		read -n1 k
-		[[ $k == [ ]] || continue
-		read -n1 k
-		if [[ $k == D ]] # \e[D left arrow
-		then
-			echo seek -10
-		elif [[ $k == C ]] # \e[C right arrow
-		then
-			echo seek +10
-		fi
-	fi
-done | /cygdrive/c/Program\ Files\ \(x86\)/SMPlayer/mplayer/mplayer.exe tmp.mp3 -slave -loop 0 -quiet >&2
+set -e -o pipefail
 
-echo bebebe
+exec 11>&1
+exec 1>&2
+
+trap 'stty echo; echo quit >&3; sleep 0.5' EXIT
+
+function player {
+	'/cygdrive/c/Program Files (x86)/SMPlayer/mplayer/mplayer.exe' -slave -loop 0 -quiet tmp.mp3 | perl -n -e 's/\r//g; if (s/\e\[A\e\[K//) {print if !/^$/} else {print}'
+}
+
+function usage {
+	echo 'Usage: h - usage, p - pause, home/left/right - seek 0/-10/+10 seconds, space - count, d - counting done, r - reset counter, q - quit'
+}
+
+function seek {
+	echo pausing_keep seek $1 $2 >&3
+}
+
+function pause {
+	if [[ ! $paused ]]
+	then
+		echo pause >&3
+		echo Paused
+		paused=1
+	fi
+}
+
+function unpause {
+	if [[ $paused ]]
+	then
+		echo pause >&3
+		echo Playing
+		paused=
+	fi
+}
+
+exec 3> >(player)
+pause >/dev/null
+sleep 1
+echo
+echo Paused
+usage
+echo
+
+bpm=0
+while true
+do
+	sleep 0.1
+	IFS='' read -p $'\r> ' -n1 -s k
+	
+	case $k in
+		'') echo ;; # enter
+		
+		p)
+			if [[ $paused ]]
+			then
+				unpause
+			else
+				pause
+			fi
+			;;
+
+		q)
+			wasPaused=$paused
+			pause >/dev/null
+			echo -n 'Quit without bpm (y|n)? '
+			while true
+			do
+				read -s -n1 k
+				case $k in
+					y) echo y; exit ;;
+					n) echo n; break
+				esac
+			done
+			[[ $wasPaused ]] || unpause >/dev/null
+			;;
+
+		$'\e')
+			read -n1 -t0.1 k || continue
+			[[ $k == [ ]]    || continue
+			read -n1 -t0.1 k || continue
+			if [[ $k == H ]] # \e[H home
+			then
+				seek 0 2
+			elif [[ $k == D ]] # \e[D left arrow
+			then
+				seek -10
+			elif [[ $k == C ]] # \e[C right arrow
+			then
+				seek +10
+			fi
+			;;
+
+		h) usage ;;
+		
+	esac
+done
+
+echo bpm >&11
 
