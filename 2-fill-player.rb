edf7500a323da8e2913493eb9a0a293c911d39c3
bdimych <bdimych@faf0b38c-ac3b-0ead-faf0-b6b3d23b80b6>
2012-02-29 00:49:39 +0000

    2-fill-player.rb command line arguments 50% done

--- 2-fill-player.rb
+++ 2-fill-player.rb
@@ -6,9 +6,89 @@ require 'lib.rb'
 
 playerDir = 'test'
 range = [150, 180]   # нужный диапазон bpm
-maxCoef = 1.2        # максимальный коефициент на который можно менять bpm (по моим впечатлением больше 1.2 песня уже слух корябит - непохожа на саму себя)
+maxCoef = 1.2        # максимальный коефициент на который можно менять bpm. По моим впечатлением больше 1.2 песня уже слух корябит - становится непохожа на саму себя
+bestOnly = false     # только лучшие песни
+groupBy = nil
+grep = nil
 
-best = false         # только лучшие песни
+
+
+log 'parsing command line'
+while ! ARGV.empty?
+	if ARGV.include? '--help'
+		log 'help requested'
+		puts
+		puts <<e
+command line options:
+
+  -b           - only best songs
+  -gd          - group resulted files by source directories
+  -gb          - group by bpm
+  -r 150-160   - needed bpm range
+  -pd /player/directory
+
+remaining argument will be used as regular expression and only matched files will be copied
+e
+		exit
+	end
+	case a = ARGV.shift
+		when '-b'
+			bestOnly = true
+		when '-gd'
+			groupBy = :dir
+		when '-gb'
+			groupBy = :bpm
+		when '-r'
+			if ARGV.shift =~ /(\d+)-(\d+)/
+				range = [$1, $2]
+			else
+				raise 'range is specified incorrectly - should be "number-number"'
+			end
+		when '-pd'
+			playerDir = ARGV.shift
+		else
+			grep = a
+	end
+end
+log 'parsing done'
+
+puts
+files = $db.keys.sort.select {|path| File.file? path and (! grep or path.match Regexp.new grep, Regexp::IGNORECASE)}
+if grep
+	puts "#{files.size} files found:"
+	puts '-----'
+	files.map {|f| puts f}
+	puts '-----'
+	puts
+end
+puts <<e
+the following parameters will be used:
+
+  player directory:          #{playerDir}
+  needed bpm range:          #{range[0]}-#{range[1]}
+  only best songs:           #{bestOnly}
+  group resulted files by:   #{
+		case groupBy
+			when :dir then 'source directories'
+			when :bpm then 'resulted bpm'
+			else 'no group, random order'
+		end
+	}
+  regular expression:        #{grep}
+  #{grep ? "regular expression was specified: \"#{grep}\", #{files.size} matched files were found (see above)" : 'no regular expression was specified, all files will be processed'}
+
+e
+exit if ! askYesNo 'is this all correct? start main loop'
+
+
+
+
+
+
+
+exit
+
+
 ARGV.each do |a|
 	case a
 		when '-b'
@@ -34,40 +114,4 @@ while true
 	end
 	sleep 1
 end
-
-=begin
-bpmDb=../bdimych.txt/bpm-database.txt
-playerDir=test
-
-set -e -o pipefail
-
-
-
-trap atexit EXIT
-function atexit {
-	echo atexit
-}
-
-
-while true
-do
-	echo main loop next iteration
-	shuf "$bpmDb" | while IFS=: read line
-	do
-		echo $line
-		if [[ $line =~ ^([-+=])?\ *(.+):\ *([0-9]+)? ]]
-		then
-			flag=${BASH_REMATCH[1]}
-			path="${BASH_REMATCH[2]}"
-			bpm=${BASH_REMATCH[3]}
-			
-		else
-			echo seems is not mp3
-		fi
-		sleep 3
-	done
-	read -p 'press Enter2'
-done
-
-=end
 
