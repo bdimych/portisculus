8690b39afde30d2c57d3f87cac4b5dcf6a2a506d
bdimych <bdimych@faf0b38c-ac3b-0ead-faf0-b6b3d23b80b6>
2012-03-02 00:04:02 +0000

    2-fill-player.rb in progress...

--- 2-fill-player.rb
+++ 2-fill-player.rb
@@ -6,8 +6,8 @@ require 'lib.rb'
 
 
 
-playerDir = 'test'
-range = [150, 180]   # нужный диапазон bpm
+playerDir = '/cygdrive/e'
+range = 150..180     # нужный диапазон bpm
 maxCoef = 1.2        # максимальный коефициент на который можно менять bpm. По моим впечатлением больше 1.2 песня уже слух корябит - становится непохожа на саму себя
 bestOnly = false     # только лучшие песни
 groupBy = nil
@@ -51,9 +51,9 @@ while ! ARGV.empty?
 			groupBy = a == '-gd' ? :dir : :bpm
 		when '-r'
 			if ARGV.shift =~ /^(\d+)-(\d+)$/
-				range = [$1.to_i, $2.to_i]
-				if range[0] > range[1]
-					usage 'first value in range is greater then second'
+				range = $1.to_i .. $2.to_i
+				if range.min == nil
+					usage 'first value in range is larger then the last'
 				end
 			else
 				usage 'range is specified incorrectly - should be "number-number"'
@@ -69,23 +69,23 @@ usage "player directory #{playerDir} does not exist" if ! File.directory? player
 log 'parsing done'
 
 puts
-files = $db.keys.sort.select do |path|
+filesToCopy = $db.keys.sort.select do |path|
 	ok = path.canBeCopied?
 	ok &&= path.match Regexp.new grep, Regexp::IGNORECASE if grep
 	ok &&= path.best? if bestOnly
 	ok
 end
 if grep or bestOnly
-	puts "#{files.size} files found:"
+	puts "#{filesToCopy.size} files found:"
 	puts '-----'
-	files.map {|f| puts f}
+	filesToCopy.map {|f| puts f}
 	puts '-----'
-	puts "#{files.size} files found"
+	puts "#{filesToCopy.size} files found"
 	puts
 end
 puts <<e
 player directory:        #{playerDir}
-needed bpm range:        #{range[0]}-#{range[1]}
+needed bpm range:        #{range.min}-#{range.max}
 only best songs:         #{bestOnly ? 'yes' : 'no'}
 group target files by:   #{
 	case groupBy
@@ -96,29 +96,72 @@ group target files by:   #{
 }
 regular expression:      #{grep ? grep : 'none'}
 e
-usage 'no files to process' if files.empty?
+usage 'no files to process' if filesToCopy.empty?
 puts
-exit if ! askYesNo 'is this correct? start main program'
-
-
-
-
+#exit if ! askYesNo 'is this correct? start main program'
+puts
+puts 'STARTING MAIN PROGRAM'
+puts
 
 
 
 exit
 
 
+
 # main program
 
 
+# reading alreadyInPlayer.txt
 
-while true
-	log 'main loop next iteration'
-	$db.keys.shuffle.each do |f|
-		next if ! f.bpmOk?
-		log "doing #{f}"
+log 'reading alreadyInPlayer.txt'
+knownNamesInPlayer = []
+if File.file? "#{playerDir}/alreadyInPlayer.txt"
+	File.open("#{playerDir}/alreadyInPlayer.txt").each do |line|
+		# /orig/path > 1234-160---Song name.mp3
+		#              ^    ^
+		#     prefix for    bpm
+		#     ordering
+		if line =~ /^(\S.+) > (\d{4}-(\d{3})---.+\S)$/
+			origPath = $1
+			nameInPlayer = $2
+			bpmInPlayer = $3
+			if $db[origPath] and File.file? "#{playerDir}/#{nameInPlayer}"
+				$db[origPath][:inPlayer] = {
+					:name => nameInPlayer,
+					:bpm => bpmInPlayer
+				}
+				knownNamesInPlayer.push nameInPlayer
+			end
+		else
+			raise "could not parse line in the alreadyInPlayer.txt: \"#{line}\""
+		end
 	end
-	sleep 1
 end
+
+
+
+
+# zeroing player directory
+
+log 'zeroing player directory'
+require 'fileutils'
+(Dir.entries(playerDir) - %w[. .. alreadyInPlayer.txt] - knownNamesInPlayer).each do |name|
+	log "deleting unknown entry #{playerDir}/#{name}"
+	FileUtils.rm_rf "#{playerDir}/#{name}"
+end
+
+
+
+# copy loop
+
+log 'copy loop'
+filesToCopy.shuffle.each_with_index do |f, i|
+	log "doing file #{i+1} from #{filesToCopy.size}: #{f}"
+#	if $db[f][:inPlayer] and 
+#	end
+end
+
+
+
 
