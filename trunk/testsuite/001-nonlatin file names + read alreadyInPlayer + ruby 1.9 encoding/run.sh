448d085a5435323bfd638347128c64e28aa31287
bdimych <bdimych@faf0b38c-ac3b-0ead-faf0-b6b3d23b80b6>
2012-12-30 04:57:48 +0000

    testsuite/001 seems done

--- trunk/testsuite/001-nonlatin file names + read alreadyInPlayer + ruby 1.9 encoding/run.sh
+++ trunk/testsuite/001-nonlatin file names + read alreadyInPlayer + ruby 1.9 encoding/run.sh
@@ -10,15 +10,39 @@ echo portdir is $portdir
 (
 	cd "$portdir"
 	rm -fv alreadyInPlayer.txt
+	echo creating alreadyInPlayer.txt
 	ls | sed 's/---\(.*\)/& < \1/' | tee alreadyInPlayer.txt
+	echo wc alreadyInPlayer.txt
 	wc alreadyInPlayer.txt
 )
 
-# TODO: проверить что енкодинги filesystem и по умолчанию не совпадают
-echo y | ruby ./2-fill-player.rb -dbf "$mydir/dbf.txt" -prd "$mydir/prd" |& tee 2-fill-out.txt &
-for x in `seq 10`
+echo starting 2-fill-player.rb
+echo y | ruby ./2-fill-player.rb -dbf "$mydir/dbf.txt" -prd "$mydir/prd" &> 2-fill-out.txt &
+echo waiting for 2-fill-out.txt
+for x in `seq 5`
 do
-	sleep 1
+	jobs
 	ls -l 2-fill-out.txt
+	sleep 1
 done
+echo looking for expected output
+if ! grep '27 known files in player, checking other files' 2-fill-out.txt
+then
+	echo ERROR: could not find expected string
+	cat 2-fill-out.txt
+	echo ERROR: could not find expected string
+	exit 1
+fi
+echo ok
+echo checking background process is ended
+jobs | tee jobs.txt
+if [[ -s jobs.txt ]]
+then
+	echo ERROR: \"jobs\" has printed something
+	exit 1
+fi
+echo ok
+echo cleaning
+rm -v "$portdir/alreadyInPlayer.txt" 2-fill-out.txt jobs.txt
+echo test done!
 
