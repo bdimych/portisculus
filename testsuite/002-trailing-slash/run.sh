d12f3aa13f7e05cf2e87cc50b55582775034f8b6
bdimych <bdimych@faf0b38c-ac3b-0ead-faf0-b6b3d23b80b6>
2013-01-02 21:26:38 +0000

    testsuite/002-trailing-slash/ in progress cygwin done

--- testsuite/002-trailing-slash/run.sh
+++ testsuite/002-trailing-slash/run.sh
@@ -2,9 +2,32 @@
 
 set -e -o pipefail
 
-сделать dbf.txt D:\Downloads\_ М У З Ы К А _\The Big Pink - Future This (2012)\
-вызвать 1-create
-проверить вывод
-	-для cygwin
-	-для linux
+./1-create-bpm-database.rb -dbf testsuite/002-trailing-slash/dbf.txt 2>&1 | tee out.txt &
+sleep 5
+
+if uname | grep -i cygwin
+then
+	echo we are in cygwin, output should contain '"cygpath result: /cygdrive/d/Downloads/_ М У З Ы К А _/The Big Pink - Future This (2012) x/"'
+	if ! grep -F 'cygpath result: /cygdrive/d/Downloads/_ М У З Ы К А _/The Big Pink - Future This (2012) x/' out.txt
+	then
+		echo ERROR: wrong output
+		exit 1
+	fi
+	echo ok
+else
+	:
+fi
+
+echo check background process is ended
+jobs >jobs.txt
+if ! grep 'Done.*1-create-bpm-database.rb' jobs.txt
+then
+	echo ERROR: unexpected \"jobs\" output:
+	cat jobs.txt
+	exit 1
+fi
+echo ok
+echo delete temp files
+rm -v out.txt jobs.txt
+echo test done!
 
