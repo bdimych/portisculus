a2fd090ca0f01b8903442549bd76658da7f8891c
bdimych <bdimych@faf0b38c-ac3b-0ead-faf0-b6b3d23b80b6>
2013-02-12 08:43:36 +0000

    testsuite/004-filtered-mode/01-range-is-also-filter/ in progress

--- trunk/testsuite/004-filtered-mode/01-range-is-also-filter/run.sh
+++ trunk/testsuite/004-filtered-mode/01-range-is-also-filter/run.sh
@@ -13,12 +13,15 @@ exec 3>&1
 {
 	echo -n yy
 	# если всё было правильно то сейчас 2-fill закончился и предлагает запустить 3-order
-опс! неправильно!
-действительно 2-fill получил два первых "yes" и пошёл дальше
-но он НЕ ЗАКОНЧИЛСЯ ЗДЕСЬ
+#опс! неправильно!
+#действительно 2-fill получил два первых "yes" и пошёл дальше
+#но он НЕ ЗАКОНЧИЛСЯ ЗДЕСЬ
 ОН ДАЛЬШЕ РАБОТАЕТ И ЗАКАНЧИВАЕТСЯ ЧЕРЕЗ КАКОЕ ТО ВРЕМЯ!
-	echo blabla >&3
-	sleep 1
+	while true
+	do
+		echo blabla `date` >&3
+		sleep 1
+	done
 	echo -n n
 } | ruby ./2-fill-player.rb -dbf testsuite/004-filtered-mode/mp3/dbf.txt -prd testsuite/004-filtered-mode/mp3/player-root/ -r172-174 -ob | tee 2-fill-log.txt
 
