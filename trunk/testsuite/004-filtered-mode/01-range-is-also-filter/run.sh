0645ba67a5dfc6fa6396bf4b754ca76df5b197e4
bdimych <bdimych@faf0b38c-ac3b-0ead-faf0-b6b3d23b80b6>
2013-02-12 08:02:33 +0000

    testsuite/004-filtered-mode/01-range-is-also-filter/ in progress

--- trunk/testsuite/004-filtered-mode/01-range-is-also-filter/run.sh
+++ trunk/testsuite/004-filtered-mode/01-range-is-also-filter/run.sh
@@ -5,18 +5,20 @@ set -e -o pipefail
 portDir=testsuite/004-filtered-mode/mp3/player-root/portisculus-1
 
 # backup
-cp -pv $portDir/alreadyInPlayer.txt .
+cp -prv $portDir{,-backup}
 ls -l $portDir | tee ls-before.txt
 
 # the test
 echo yyn | ./2-fill-player.rb -dbf testsuite/004-filtered-mode/mp3/dbf.txt -prd testsuite/004-filtered-mode/mp3/player-root/ -r172-174 -ob | tee 2-fill-log.txt
 
-# clear and check
-rm -fv $portDir/????-17{2,3,4}---*
-cp -pv alreadyInPlayer.txt $portDir
+# restore
+rm -rfv $portDir
+mv -v $portDir{-backup,}
+# check restored
 ls -l $portDir | tee ls-after.txt
 diff ls-before.txt ls-after.txt
-rm -v alreadyInPlayer.txt ls-before.txt ls-after.txt 2-fill-log.txt
+# clear
+rm -v ls-before.txt ls-after.txt 2-fill-log.txt
 
 # ok!
 echo success! test passed!
