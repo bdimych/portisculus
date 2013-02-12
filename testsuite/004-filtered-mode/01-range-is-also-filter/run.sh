b582af5c74433c7fe49094827b18f2297f8b81a7
bdimych <bdimych@faf0b38c-ac3b-0ead-faf0-b6b3d23b80b6>
2013-02-12 08:39:47 +0000

    testsuite/004-filtered-mode/01-range-is-also-filter/ in progress

--- testsuite/004-filtered-mode/01-range-is-also-filter/run.sh
+++ testsuite/004-filtered-mode/01-range-is-also-filter/run.sh
@@ -8,8 +8,19 @@ portDir=testsuite/004-filtered-mode/mp3/player-root/portisculus-1
 cp -prv $portDir{,-backup}
 ls -l $portDir | tee ls-before.txt
 
+exec 3>&1
 # the test
-echo -n yyn | ruby ./2-fill-player.rb -dbf testsuite/004-filtered-mode/mp3/dbf.txt -prd testsuite/004-filtered-mode/mp3/player-root/ -r172-174 -ob | tee 2-fill-log.txt
+{
+	echo -n yy
+	# если всё было правильно то сейчас 2-fill закончился и предлагает запустить 3-order
+опс! неправильно!
+действительно 2-fill получил два первых "yes" и пошёл дальше
+но он НЕ ЗАКОНЧИЛСЯ ЗДЕСЬ
+ОН ДАЛЬШЕ РАБОТАЕТ И ЗАКАНЧИВАЕТСЯ ЧЕРЕЗ КАКОЕ ТО ВРЕМЯ!
+	echo blabla >&3
+	sleep 1
+	echo -n n
+} | ruby ./2-fill-player.rb -dbf testsuite/004-filtered-mode/mp3/dbf.txt -prd testsuite/004-filtered-mode/mp3/player-root/ -r172-174 -ob | tee 2-fill-log.txt
 
 # restore
 rm -rfv $portDir
