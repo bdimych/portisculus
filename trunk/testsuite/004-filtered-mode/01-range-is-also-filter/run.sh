d2da92067e803f7fefed9b047a1b310bec90a886
bdimych <bdimych@faf0b38c-ac3b-0ead-faf0-b6b3d23b80b6>
2013-02-13 21:24:46 +0000

    testsuite/004-filtered-mode/01-range-is-also-filter/ in progress

--- trunk/testsuite/004-filtered-mode/01-range-is-also-filter/run.sh
+++ trunk/testsuite/004-filtered-mode/01-range-is-also-filter/run.sh
@@ -3,36 +3,23 @@
 set -e -o pipefail
 
 portDir=testsuite/004-filtered-mode/mp3/player-root/portisculus-1
+portDir2=testsuite/004-filtered-mode/mp3/player-root/portisculus-2
 
 # backup
 cp -prv $portDir{,-backup}
 ls -l $portDir | tee ls-before.txt
 
-exec 3>&1
-# the test
-{
-	echo -n yy
-	# если всё было правильно то сейчас 2-fill закончился и предлагает запустить 3-order
-#опс! неправильно!
-#действительно 2-fill получил два первых "yes" и пошёл дальше
-#но он НЕ ЗАКОНЧИЛСЯ ЗДЕСЬ
-ОН ДАЛЬШЕ РАБОТАЕТ И ЗАКАНЧИВАЕТСЯ ЧЕРЕЗ КАКОЕ ТО ВРЕМЯ!
-	while true
-	do
-		echo blabla `date` >&3
-		sleep 1
-	done
-	echo -n n
-} | ruby ./2-fill-player.rb -dbf testsuite/004-filtered-mode/mp3/dbf.txt -prd testsuite/004-filtered-mode/mp3/player-root/ -r172-174 -ob | tee 2-fill-log.txt
+echo -n yyyy | ruby ./2-fill-player.rb -dbf testsuite/004-filtered-mode/mp3/dbf.txt -prd testsuite/004-filtered-mode/mp3/player-root/ -r172-174 -ob | tee test-log.txt
+ls -U $portDir2 | tee ls-test.txt
 
 # restore
-rm -rfv $portDir
+rm -rfv $portDir2
 mv -v $portDir{-backup,}
 # check restored
 ls -l $portDir | tee ls-after.txt
 diff ls-before.txt ls-after.txt
 # clear
-rm -v ls-before.txt ls-after.txt 2-fill-log.txt
+rm -v ls-before.txt ls-test.txt ls-after.txt test-log.txt
 
 # ok!
 echo success! test passed!
