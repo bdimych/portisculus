e9c56687022c854f7bb5dda0d0a174e8777ca4ca
bdimych <bdimych@faf0b38c-ac3b-0ead-faf0-b6b3d23b80b6>
2013-02-26 12:17:45 +0000

    testsuite/004-filtered-mode/01-range-is-also-filter/ in progress

--- testsuite/004-filtered-mode/01-range-is-also-filter/run.sh
+++ testsuite/004-filtered-mode/01-range-is-also-filter/run.sh
@@ -9,11 +9,15 @@ portDir2=testsuite/004-filtered-mode/mp3/player-root/portisculus-2
 cp -prv $portDir{,-backup}
 ls -l $portDir | tee ls-before.txt
 
-echo -n yyyy | ruby ./2-fill-player.rb -dbf testsuite/004-filtered-mode/mp3/dbf.txt -prd testsuite/004-filtered-mode/mp3/player-root/ -r172-174 -ob | tee test-log.txt
-ls -U $portDir2 | tee ls-test.txt
+
+
+echo -n yyn | ruby ./2-fill-player.rb -dbf testsuite/004-filtered-mode/mp3/dbf.txt -prd testsuite/004-filtered-mode/mp3/player-root/ -r172-174 -ob | tee test-log.txt
+ls -U $portDir | tee ls-test.txt
+
+
 
 # restore
-rm -rfv $portDir2
+rm -rfv $portDir $portDir2
 mv -v $portDir{-backup,}
 # check restored
 ls -l $portDir | tee ls-after.txt
