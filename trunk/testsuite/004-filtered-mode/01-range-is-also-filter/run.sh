a4fbb9edbed7299441de5732d6d714abc59be2c7
bdimych <bdimych@faf0b38c-ac3b-0ead-faf0-b6b3d23b80b6>
2013-03-06 12:37:05 +0000

    testsuite/004-filtered-mode/01-range-is-also-filter/ in progress

--- trunk/testsuite/004-filtered-mode/01-range-is-also-filter/run.sh
+++ trunk/testsuite/004-filtered-mode/01-range-is-also-filter/run.sh
@@ -34,6 +34,8 @@ do
 	grep "$nameRegexp" ls-test.txt
 done
 set -x
+grep '0001-168---03 - Малинки\.mp3' ls-test.txt
+grep '0008-166---302 - Jack and the Rave_1\.mp3' ls-test.txt
 grep '5 added (2 recodedFromThePlayerDirItself):' test-log.txt
 grep 'saved, 20 lines' test-log.txt
 set +x
