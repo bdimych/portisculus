1fa09774bdfe05d51989f148abb66e3405b6f543
bdimych <bdimych@faf0b38c-ac3b-0ead-faf0-b6b3d23b80b6>
2013-04-04 18:16:02 +0000

    testsuite/005-2fill-raise-if-target-file-already-exists/ in progress

--- testsuite/005-2fill-raise-if-target-file-already-exists/run.sh
+++ testsuite/005-2fill-raise-if-target-file-already-exists/run.sh
@@ -4,15 +4,30 @@ set -e -o pipefail
 
 mkdir portisculus-1
 
-# symlink
-ln -s ./linked.mp3 'portisculus-1/0000-BLS---Уже есть такой файл Mexico JD.mp3'
-echo ynyn | ruby 2-fill-player.rb -prd . -dbf ./testsuite/005-2fill-raise-if-target-file-already-exists/dbf.txt
+trgFile='portisculus-1/0000-BLS---Уже есть такой файл Mexico JD.mp3'
 
-# fifo
-# folder
-# common file
+function runAndCheck {
+	if echo ynyn | ruby 2-fill-player.rb -prd . -dbf ./testsuite/005-2fill-raise-if-target-file-already-exists/dbf.txt | tee test-log.txt
+	then
+		echo ERROR: 2fill should fail but it succeeded
+		exit 1
+	fi
+	echo check test-log.txt
+	grep -F "target file $trgFile already exists" test-log.txt
+	echo ok
+}
 
-rm -rfv portisculus-1
+echo - - - symlink - - -
+ln -s linked.mp3 "$trgFile"
+runAndCheck
+
+echo - - - fifo - - -
+
+echo - - - folder - - -
+
+echo - - - common file - - -
+
+rm -rfv portisculus-1 test-log.txt
 
 echo success!
 
