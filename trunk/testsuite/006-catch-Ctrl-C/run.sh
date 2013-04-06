00703bcdb118ffe4fd9a1d57c02c3bdf6fe6dc80
bdimych <bdimych@faf0b38c-ac3b-0ead-faf0-b6b3d23b80b6>
2013-04-06 13:57:28 +0000

    testsuite/006-catch-Ctrl-C/ in progress

--- trunk/testsuite/006-catch-Ctrl-C/run.sh
+++ trunk/testsuite/006-catch-Ctrl-C/run.sh
@@ -2,9 +2,16 @@
 
 set -e -o pipefail
 
-echo -n yyn | ruby 2-fill-player.rb -dbf testsuite/006-catch-Ctrl-C/dbf.txt -prd . -r 148-166
+echo -n yyn | ruby 2-fill-player.rb -dbf testsuite/006-catch-Ctrl-C/dbf.txt -prd . -r 148-166 | tee test-log.txt
+msgCount=$(sed -n '/preparing for adding loop/,/---- at_exit ok -----/p' test-log.txt | wc -l)
+echo msgCount is $msgCount
+if (( $msgCount < 80 ))
+then
+	echo ERROR: the log is too short $msgCount lines
+	exit 1
+fi
 
-rm -rv portisculus-1
+rm -rv portisculus-1 test-log.txt
 
 echo ok, test passed
 
