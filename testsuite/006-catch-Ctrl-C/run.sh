dea71ffd844ba642e713f3d5d3bd2ce11344fdc8
bdimych <bdimych@faf0b38c-ac3b-0ead-faf0-b6b3d23b80b6>
2013-04-06 14:03:53 +0000

    testsuite/006-catch-Ctrl-C/ in progress

--- testsuite/006-catch-Ctrl-C/run.sh
+++ testsuite/006-catch-Ctrl-C/run.sh
@@ -3,9 +3,9 @@
 set -e -o pipefail
 
 echo -n yyn | ruby 2-fill-player.rb -dbf testsuite/006-catch-Ctrl-C/dbf.txt -prd . -r 148-166 | tee test-log.txt
-msgCount=$(sed -n '/preparing for adding loop/,/---- at_exit ok -----/p' test-log.txt | grep '^[' | wc -l)
+msgCount=$(sed -n '/preparing for adding loop/,/---- at_exit ok -----/p' test-log.txt | grep '^\[' | wc -l)
 echo msgCount is $msgCount
-if (( $msgCount < 80 ))
+if (( $msgCount < 65 ))
 then
 	echo ERROR: the log is too short $msgCount lines
 	exit 1
