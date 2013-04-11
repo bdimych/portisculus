5c18a19618b8733060c90eb95b3e7428a0977690
bdimych <bdimych@faf0b38c-ac3b-0ead-faf0-b6b3d23b80b6>
2013-04-11 12:18:45 +0000

    testsuite/006-catch-Ctrl-C/ in progress

--- testsuite/006-catch-Ctrl-C/2fill-wrapper.rb
+++ testsuite/006-catch-Ctrl-C/2fill-wrapper.rb
@@ -2,16 +2,29 @@ msgCount = ARGV[0].to_i
 if msgCount <= 0
 	raise "ERROR: usage: #$0 msgCount"
 end
-puts "msgCount is #{msgCount}"
+totalSec = 10
+$msgDelay = totalSec.to_f/msgCount
+puts "msgCount: #{msgCount}, totalSec: #{totalSec}, msgDelay: #$msgDelay"
+
+srand
 
 module Kernel
 	alias :realputs :puts
 	def puts(*args)
-		realputs 'aaa'
 		realputs *args
-		realputs 'bbb'
+		if args[0] =~ /preparing for adding loop$/
+			puts 'start delaying'
+			$doDelay = true
+		end
+		if $doDelay
+			sleep $msgDelay
+		end
 	end
 end
 
 require './2-fill-player.rb'
+
+if not $doDelay
+	raise '$doDelay was not set'
+end
 
