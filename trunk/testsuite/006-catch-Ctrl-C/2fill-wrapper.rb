ee31a7ec03e76cefb3b6de296d7a2fadac8da630
bdimych <bdimych@faf0b38c-ac3b-0ead-faf0-b6b3d23b80b6>
2013-04-12 15:43:39 +0000

    testsuite/006-catch-Ctrl-C/ in progress [NOT READY!]

--- trunk/testsuite/006-catch-Ctrl-C/2fill-wrapper.rb
+++ trunk/testsuite/006-catch-Ctrl-C/2fill-wrapper.rb
@@ -2,11 +2,27 @@ msgCount = ARGV[0].to_i
 if msgCount <= 0
 	raise "ERROR: usage: #$0 msgCount"
 end
+
+STDOUT.sync = true
+
 totalSec = 10
+if chld = fork
+	puts "forked #{chld} from #$$"
+	srand
+	waitSec = 2 + rand * totalSec # если без добавки то всё таки слишком мало получается сигнал раньше чем создаётся at_exit
+	puts "waitSec #{waitSec}"
+	sleep waitSec
+	puts "sending INT to #{chld}"
+	Process.kill 'INT', chld
+p Process.wait
+p $?
+	exit
+end
+
+puts Process.ppid
+
 $msgDelay = totalSec.to_f/msgCount
 puts "msgCount: #{msgCount}, totalSec: #{totalSec}, msgDelay: #$msgDelay"
-
-srand
 
 module Kernel
 	alias :realputs :puts
