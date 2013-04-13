c81097161d9f67bff7d1cfcef713ce8fd73daff2
bdimych <bdimych@faf0b38c-ac3b-0ead-faf0-b6b3d23b80b6>
2013-04-13 08:04:38 +0000

    small bug fixed

--- trunk/2-fill-player.rb
+++ trunk/2-fill-player.rb
@@ -312,35 +312,39 @@ filesToAdd.shuffle.each_with_index do |f, i|
 
 		FileUtils.copy_entry f, './tmp.mp3', false, false, true
 
+# здесь не перехватывать ctrl-c а вернуть по умолчанию сразу выход
+# просто чтобы не ждать пока долго перекодирует
+# при этом никакой логической ошибки не будет т.к. копировать на плеер ещё не начинали
 intTrap = trap 'INT', 'DEFAULT'
 
 		log (cmd = %w(lame --decode tmp.mp3 tmp-decoded.wav)).join ' '
 		raise 'error decoding mp3' if ! system *cmd
-							#
-							# как то раз обнаружил на плеере файлы размером всего по несколько килобайт
-							# оказалось это lame ошибается но возвращает ноль:
-							#
-							# bdimych@bdimych-win7 ~/portisculus
-							# $ lame --decode tmp.mp3 tmp-decoded.wav ; echo $?
-							# input:  tmp.mp3  (16 kHz, 1 channel, MPEG-2 Layer II)
-							# output: tmp-decoded.wav  (16 bit, Microsoft WAVE)
-							# skipping initial 241 samples (encoder+decoder delay)
-							# Frame#     2/4325   160 kbps         hip: bitstream problem, resyncing skipping 1818 bytes...
-							# Frame#     3/4325    96 kbps         hip: bitstream problem, resyncing skipping 172184 bytes...
-							# Frame#     4/4325   144 kbps         hip: bitstream problem, resyncing skipping 21274 bytes...
-							# Frame#     5/4325   128 kbps         hip: bitstream problem, resyncing skipping 541 bytes...
-							# Error: sample frequency has changed in MP3 file - not supported
-							#
-							# 0
-							#
-							# этот баг есть в гугле
-							# http://mp3-encoding.31853.n2.nabble.com/lame-decode-failing-to-abort-on-garbage-files-td34649.html
-							# но ещё не исправлен (2012-04-03-03-22-50 версия lame 3.99.5)
-							#
-							# поэтому вот ещё простейшая проверка на размер
+		#
+		# как то раз обнаружил на плеере файлы размером всего по несколько килобайт
+		# оказалось это lame ошибается но возвращает ноль:
+		#
+		# bdimych@bdimych-win7 ~/portisculus
+		# $ lame --decode tmp.mp3 tmp-decoded.wav ; echo $?
+		# input:  tmp.mp3  (16 kHz, 1 channel, MPEG-2 Layer II)
+		# output: tmp-decoded.wav  (16 bit, Microsoft WAVE)
+		# skipping initial 241 samples (encoder+decoder delay)
+		# Frame#     2/4325   160 kbps         hip: bitstream problem, resyncing skipping 1818 bytes...
+		# Frame#     3/4325    96 kbps         hip: bitstream problem, resyncing skipping 172184 bytes...
+		# Frame#     4/4325   144 kbps         hip: bitstream problem, resyncing skipping 21274 bytes...
+		# Frame#     5/4325   128 kbps         hip: bitstream problem, resyncing skipping 541 bytes...
+		# Error: sample frequency has changed in MP3 file - not supported
+		#
+		# 0
+		#
+		# этот баг есть в гугле
+		# http://mp3-encoding.31853.n2.nabble.com/lame-decode-failing-to-abort-on-garbage-files-td34649.html
+		# но ещё не исправлен (2012-04-03-03-22-50 версия lame 3.99.5)
+		#
+		# поэтому вот ещё простейшая проверка на размер
 		if 100000 > File.size('tmp-decoded.wav')
 			wrn 'tmp-decoded.wav is too small, probably lame failed'
 			lameDecodeProblem[f] = File.size('tmp-decoded.wav')
+trap 'INT', intTrap
 			next
 		end
 
