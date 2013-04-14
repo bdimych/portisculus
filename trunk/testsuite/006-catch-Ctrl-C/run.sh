affde2ebe27e0659bfb956b02355ecd4100e17e8
bdimych <bdimych@faf0b38c-ac3b-0ead-faf0-b6b3d23b80b6>
2013-04-14 19:10:10 +0000

    testsuite/006-catch-Ctrl-C/ done

--- trunk/testsuite/006-catch-Ctrl-C/run.sh
+++ trunk/testsuite/006-catch-Ctrl-C/run.sh
@@ -15,7 +15,7 @@ then
 	echo ERROR: log is too short $msgCount lines
 	exit 1
 fi
-echo check test-log.txt
+echo check test-log.txt 1
 grep '^3 added (0 tempCopy):$' test-log.txt
 [[ $(ls portisculus-1/*.mp3 | wc -l) == 3 ]]
 
@@ -23,6 +23,11 @@ rm -rv portisculus-1 test-log.txt
 
 echo second slow run
 myRun testsuite/006-catch-Ctrl-C/2fill-wrapper.rb $msgCount
+echo check test-log.txt 2
+grep 'exit cause of Ctrl-C was caught$' test-log.txt
+grep '^2-fill finished correctly$' test-log.txt
+grep -- '------------------------------ at_exit ok ------------------------------$' test-log.txt
+grep -- '- - - - - - - - - - - - - - - - - - - - - - - - - !!! Ctrl-C caught !!! - will stop at the nearest appropriate moment$' test-log.txt
 
 rm -rv portisculus-1 test-log.txt
 
