41fbc7ece762118c7b6b294c368feb8679ef0b23
bdimych <bdimych@faf0b38c-ac3b-0ead-faf0-b6b3d23b80b6>
2013-05-28 12:01:46 +0000

    testsuite/007-problematic-mp3-files/ in progress

---
+++ trunk/testsuite/007-problematic-mp3-files/run.sh
@@ -0,0 +1,12 @@
+cat >$0.dbf.txt <<dbf
+testsuite/007-problematic-mp3-files/mp3/02 - Gmo - Koiau (Exclusive Track).mp3				:141
+testsuite/007-problematic-mp3-files/mp3/04. Communiqué.mp3														:byhands
+testsuite/007-problematic-mp3-files/mp3/06 - Mantrix - Gaia.mp3												:142
+testsuite/007-problematic-mp3-files/mp3/10 - Cosma - Time Has Come.mp3								:143
+testsuite/007-problematic-mp3-files/mp3/11. Недетское Время.mp3												:byhands
+testsuite/007-problematic-mp3-files/mp3/13 - S.u.n. Project - Hangin' Around.mp3			:144
+testsuite/007-problematic-mp3-files/mp3/13.DJ Sim - Happy Organ.mp3										:145
+testsuite/007-problematic-mp3-files/mp3/17 - Psypsiq Jiouri - Histora De Un Sueno.mp3	:146
+testsuite/007-problematic-mp3-files/mp3/19 - Syn Sun - Ceremony.mp3										:147
+dbf
+
