fe730e2ce3abe363d88718c48702378454685c07
bdimych <bdimych@faf0b38c-ac3b-0ead-faf0-b6b3d23b80b6>
2013-06-02 06:36:22 +0000

    testsuite/007-problematic-mp3-files/ in progress 1create test done

--- trunk/testsuite/007-problematic-mp3-files/run.sh
+++ trunk/testsuite/007-problematic-mp3-files/run.sh
@@ -16,16 +16,39 @@ testsuite/007-problematic-mp3-files/mp3/17 - Psypsiq Jiouri - Histora De Un Suen
 testsuite/007-problematic-mp3-files/mp3/19 - Syn Sun - Ceremony.mp3										:147
 dbf
 
-					dbf=tmp.txt
-					echo 'testsuite/007-problematic-mp3-files/mp3/19 - Syn Sun - Ceremony.mp3: byhands' >$dbf
-
 # test
-perl -e '$| = 1; print "l\r"; sleep 6; for (1..75) {print " "; select undef, undef, undef, 0.5} sleep 2; print "d\n\r"' | ruby ./1-create-bpm-database.rb -dbf $dbf 2>&1 | tee test-log.txt
+perl -e '
+	$| = 1;
+	print "l\r";
+	sleep 6;
+	for (1..2) {
+		for (1..75) {
+			print " ";
+			select undef, undef, undef, 0.5
+		}
+		sleep 1;
+		print "d\n\r";
+		sleep 3
+	}
+' | ruby ./1-create-bpm-database.rb -dbf $dbf 2>&1 | tee test-log.txt
 # check
 set -x
 grep -F '2 files remains without bpm, count them by hands (Y, n, (l)ist)? y' test-log.txt
+grep -F '[byhands] testsuite/007-problematic-mp3-files/mp3/04. Communiqué.mp3' test-log.txt
+grep -F '[byhands] testsuite/007-problematic-mp3-files/mp3/11. Недетское Время.mp3' test-log.txt
+grep -P '^\QPlaying 04. Communiqué.mp3.' test-log.txt
+grep -P '^\QPlaying 11. Недетское Время.mp3.' test-log.txt
+[[ $(grep '^Starting playback...$' test-log.txt | wc -l) == 2 ]]
+[[ $(grep -P '^Counter:  74;   seconds: 37;   bpm: 1(19|20|21);   average bpm result: 1(19|20)$' test-log.txt | wc -l) == 2 ]]
+[[ $(grep '^Exiting... (Quit)$' test-log.txt | wc -l) == 2 ]]
+[[ $(grep -P 'by hands result: "1(19|20)"$' test-log.txt | wc -l) == 2 ]]
+[[ $(grep -P '^bpm = 1(19|20)$' test-log.txt | wc -l) == 2 ]]
+tail -n2 test-log.txt | grep -F 'skip writing, db was not changed: {:totalPaths=>9, :nonexistent=>0, :dirs=>0, :files=>9, :best=>0, :beatless=>0, :skipped=>0, :withoutBpm=>0, :canBeAdded=>9}'
+tail -n2 test-log.txt | grep -F 'the end: nil'
+grep -P '04. Communiqué.mp3: 1(19|20)$' $dbf
+grep -P '11. Недетское Время.mp3: 1(19|20)$' $dbf
 
-rm -v $dbf                               # test-log.txt
+rm -v $dbf test-log.txt
 
 echo ok! test done! ':)'
 
