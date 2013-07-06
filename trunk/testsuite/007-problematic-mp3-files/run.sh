4e5b98e69f495fde5d3227877676dca43af0dc61
bdimych <bdimych@faf0b38c-ac3b-0ead-faf0-b6b3d23b80b6>
2013-07-06 10:35:29 +0000

    testsuite/007-problematic-mp3-files/ in progress

--- trunk/testsuite/007-problematic-mp3-files/run.sh
+++ trunk/testsuite/007-problematic-mp3-files/run.sh
@@ -14,6 +14,13 @@ testsuite/007-problematic-mp3-files/mp3/13 - S.u.n. Project - Hangin' Around.mp3
 `pwd`/testsuite/007-problematic-mp3-files/mp3/13.DJ Sim - Happy Organ.mp3							:145
 testsuite/007-problematic-mp3-files/mp3/17 - Psypsiq Jiouri - Histora De Un Sueno.mp3	:146
 `pwd`/testsuite/007-problematic-mp3-files/mp3/19 - Syn Sun - Ceremony.mp3							:147
+
+      ./testsuite/007-problematic-mp3-files/mp3/lame-decode-error/04.D'Ohm Feat. Tofke & Insider - Mushrooms.mp3: 155
+  `pwd`/testsuite/007-problematic-mp3-files/mp3/lame-decode-error/06.Citadel Of Kaos Featuring Dave Jay - Show Me Love.mp3: 159
+      ./testsuite/007-problematic-mp3-files/mp3/lame-decode-error/09.The Prophet - Big Boys Don't Cry (Rave Mix).mp3:									160
+ `pwd`//testsuite/007-problematic-mp3-files/mp3/lame-decode-error/09.Wicked Dimension - Everytime I Think Of You.mp3: 172
+      ./testsuite/007-problematic-mp3-files/mp3/lame-decode-error/10.DJ Waxweazle - techno hits ya.mp3:																160
+`pwd`/./testsuite/007-problematic-mp3-files/mp3/lame-decode-error/15.UK - heaven.mp3:																									160
 dbf
 
 
@@ -46,7 +53,7 @@ grep -P '^\QPlaying 11. Недетское Время.mp3.' test-log.txt
 [[ $(grep '^Exiting... (Quit)$' test-log.txt | wc -l) == 2 ]]
 [[ $(grep -P 'by hands result: "1(19|20)"$' test-log.txt | wc -l) == 2 ]]
 [[ $(grep -P '^bpm = 1(19|20)$' test-log.txt | wc -l) == 2 ]]
-tail -n2 test-log.txt | grep -F 'skip writing, db was not changed: {:totalPaths=>9, :nonexistent=>0, :dirs=>0, :files=>9, :best=>0, :beatless=>0, :skipped=>0, :withoutBpm=>0, :canBeAdded=>9}'
+tail -n2 test-log.txt | grep -F 'skip writing, db was not changed: {:totalPaths=>15, :nonexistent=>0, :dirs=>0, :files=>15, :best=>0, :beatless=>0, :skipped=>0, :withoutBpm=>0, :canBeAdded=>15}'
 tail -n2 test-log.txt | grep -F 'the end: nil'
 grep -P '04. Communiqué.mp3: 1(19|20)$' $dbf
 grep -P '11. Недетское Время.mp3: 1(19|20)$' $dbf
@@ -63,9 +70,9 @@ echo -n yyn | ruby 2-fill-player.rb -dbf $dbf -prd $prd |& tee test-log.txt
 # check
 set -x
 grep '^there is no portisculus directory in player root so going to create' test-log.txt
-grep 'doing file . of 9 (added .): testsuite/007-problematic-mp3-files/mp3/13.DJ Sim - Happy Organ.mp3' -A2 test-log.txt | grep -F '227 sec (3 min 47 sec) - ok'
+grep "doing file [0-9]\+ of 15 (added [0-9]\+): $(pwd)/testsuite/007-problematic-mp3-files/mp3/13.DJ Sim - Happy Organ.mp3" -A2 test-log.txt | grep -F '227 sec (3 min 47 sec) - ok'
 grep -F -- '------------------------------ at_exit ok ------------------------------' test-log.txt
-grep '^9 added (0 tempCopy):$' test-log.txt
+grep '^15 added (0 tempCopy):$' test-log.txt
 grep '^0 tooLong:$' test-log.txt
 [[ $(wc -l $prd/portisculus-1/alreadyInPlayer.txt) == 9 ]]
 set +x
