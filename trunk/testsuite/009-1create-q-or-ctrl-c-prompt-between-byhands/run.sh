45ce592207dca034567645316461f06d0e780a76
bdimych <bdimych@faf0b38c-ac3b-0ead-faf0-b6b3d23b80b6>
2013-08-04 14:26:58 +0000

    testsuite/009-1create-q-or-ctrl-c-prompt-between-byhands/ in progress

--- trunk/testsuite/009-1create-q-or-ctrl-c-prompt-between-byhands/run.sh
+++ trunk/testsuite/009-1create-q-or-ctrl-c-prompt-between-byhands/run.sh
@@ -7,9 +7,26 @@ touch second-dummy-file.mp3
 echo silence-30-sec.mp3: byhands >dbf.txt
 echo second-dummy-file.mp3: soundstretchFailed >>dbf.txt
 
-{ echo -n y; sleep 10; echo -n =y; } | ruby ./1-create-bpm-database.rb -dbf dbf.txt
+mkfifo fifo.txt
+ruby ./1-create-bpm-database.rb -dbf dbf.txt <fifo.txt |& tee test-log.txt &
+testCmdPid=$!
 
-rm -v silence-30-sec.mp3 second-dummy-file.mp3 dbf.txt
+sleep 3
+echo $!
+ps
+sleep 3
+{
+	echo -n y
+	sleep 10
+	echo -n =y
+} >fifo.txt
+
+set -x
+jobs -l
+wait $testCmdPid
+echo $?
+
+rm -v silence-30-sec.mp3 second-dummy-file.mp3 dbf.txt fifo.txt test-log.txt
 
 echo ok, $0 done
 
