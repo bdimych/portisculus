c1883ecba0f614de5eb2784c6a3f1e5ab1f13058
bdimych <bdimych@faf0b38c-ac3b-0ead-faf0-b6b3d23b80b6>
2013-08-04 13:55:54 +0000

    testsuite/009-1create-q-or-ctrl-c-prompt-between-byhands/ in progress

--- testsuite/009-1create-q-or-ctrl-c-prompt-between-byhands/run.sh
+++ testsuite/009-1create-q-or-ctrl-c-prompt-between-byhands/run.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 
+set -e -o pipefail
+
+ffmpeg -n -f lavfi -i aevalsrc=0 -t 30 silence-30-sec.mp3
+touch second-dummy-file.mp3
+echo silence-30-sec.mp3: byhands >dbf.txt
+echo second-dummy-file.mp3: soundstretchFailed >>dbf.txt
+
+{ echo -n y; sleep 10; echo -n =y; } | ruby ./1-create-bpm-database.rb -dbf dbf.txt
+
+rm -v silence-30-sec.mp3 second-dummy-file.mp3 dbf.txt
+
+echo ok, $0 done
+
