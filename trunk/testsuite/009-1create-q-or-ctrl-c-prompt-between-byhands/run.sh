5d4069185ff66b47d6cdc1312dd911a7f5b3fcd3
bdimych <bdimych@faf0b38c-ac3b-0ead-faf0-b6b3d23b80b6>
2013-08-05 11:19:59 +0000

    testsuite/009-1create-q-or-ctrl-c-prompt-between-byhands/ in progress

--- trunk/testsuite/009-1create-q-or-ctrl-c-prompt-between-byhands/run.sh
+++ trunk/testsuite/009-1create-q-or-ctrl-c-prompt-between-byhands/run.sh
@@ -4,29 +4,57 @@ set -e -o pipefail
 
 ffmpeg -n -f lavfi -i aevalsrc=0 -t 30 silence-30-sec.mp3
 touch second-dummy-file.mp3
+
+
+echo
+echo = = = = = = = = = = = = = = = = = = = = check q = = = = = = = = = = = = = = = = = = = =
+echo
+
 echo silence-30-sec.mp3: byhands >dbf.txt
 echo second-dummy-file.mp3: soundstretchFailed >>dbf.txt
 
-mkfifo test-input-fifo.txt
-ruby ./1-create-bpm-database.rb -dbf dbf.txt <test-input-fifo.txt |& tee test-log.txt &
-testCmdPid=$!
-
-sleep 3
-echo $!
-ps
-sleep 3
 {
 	echo -n y
 	sleep 10
-	echo -n =y
-} >test-input-fifo.txt
+	echo -n =
+	sleep 1
+	echo -n y
+	sleep 3
+	echo >&2
+	tail -n 1 test-log.txt | grep -F 'press any key to continue or "q" or ctrl-c to quit' >&2
+	echo -n q
+} | ruby ./1-create-bpm-database.rb -dbf dbf.txt |& tee test-log.txt &
 
-set -x
-jobs -l
-wait $testCmdPid
-echo $?
+wait %1
 
-rm -v silence-30-sec.mp3 second-dummy-file.mp3 dbf.txt test-input-fifo.txt test-log.txt
+
+
+
+echo
+echo = = = = = = = = = = = = = = = = = = = = check int = = = = = = = = = = = = = = = = = = = =
+echo
+
+
+
+
+# sleep 2
+# echo ps
+# ps
+# echo jobs
+# jobs -l | tee jobs.txt
+# sleep 14
+
+# wait %1
+
+
+	# else
+		# read rubyPid rest < <(grep '[0-9]\+ \+| ruby ./1-create-bpm-database.rb' jobs.txt)
+		# echo rubyPid is $rubyPid >&2
+		# kill -INT $rubyPid
+	# fi
+
+
+rm -v silence-30-sec.mp3 second-dummy-file.mp3 dbf.txt test-log.txt
 
 echo ok, $0 done
 
