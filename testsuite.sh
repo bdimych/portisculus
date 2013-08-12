8b2af05f46448721415102688f4f5a19cd518e0d
bdimych <bdimych@faf0b38c-ac3b-0ead-faf0-b6b3d23b80b6>
2013-08-12 13:26:19 +0000

    testsuite.sh fixed for the 009 test

--- testsuite.sh
+++ testsuite.sh
@@ -2,12 +2,14 @@
 
 set -e -o pipefail
 
+tty=$(tty)
 find ./testsuite -name run.sh | while read f
 do
 	echo - - - - - doing test: "$f" - - - - -
-	"$f"
+	"$f" <$tty
 	echo test passed "$f"
 	echo
 done
+
 echo testsuite succeeded!
 
