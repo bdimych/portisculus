a0c9bcbf02c85bf348a37851a314d722d13c21b2
bdimych <bdimych@faf0b38c-ac3b-0ead-faf0-b6b3d23b80b6>
2013-10-27 20:05:11 +0000

    2fill always random bpm in progress

--- trunk/testsuite/010-always-random-bpm/run.sh
+++ trunk/testsuite/010-always-random-bpm/run.sh
@@ -2,7 +2,7 @@
 
 set -e -o pipefail
 
-ruby 2-fill-player.rb -dbf testsuite/010-always-random-bpm/база-бпм.txt -prd testsuite/010-always-random-bpm/плеер-рут | tee лог.текст
+echo -n yyn | ruby 2-fill-player.rb -dbf testsuite/010-always-random-bpm/база-бпм.txt -prd testsuite/010-always-random-bpm/плеер-рут -r100-200 | tee лог.текст
 
 rm -rv testsuite/010-always-random-bpm/плеер-рут/portisculus-1 лог.текст
 
