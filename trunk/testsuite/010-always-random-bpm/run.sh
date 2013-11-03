7ae729f493144b6fa58cdee51b539b705f30fb71
bdimych <bdimych@faf0b38c-ac3b-0ead-faf0-b6b3d23b80b6>
2013-11-03 10:48:15 +0000

    2fill always random bpm done

--- trunk/testsuite/010-always-random-bpm/run.sh
+++ trunk/testsuite/010-always-random-bpm/run.sh
@@ -2,7 +2,12 @@
 
 set -e -o pipefail
 
-echo -n yyn | ruby 2-fill-player.rb -dbf testsuite/010-always-random-bpm/база-бпм.txt -prd testsuite/010-always-random-bpm/плеер-рут -r100-200 | tee лог.текст
+echo -n yyn | ruby 2-fill-player.rb -dbf testsuite/010-always-random-bpm/база-бпм.txt -prd testsuite/010-always-random-bpm/плеер-рут -r100-200 -arbpm | tee лог.текст
+
+set -x
+[[ $(grep 'get new bpm because arbpm flag is set' лог.текст | wc -l) == 3 ]]
+[[ $(grep '^\[.\+\] ffmpeg -nostdin -y -i tmp.mp3 -ab 128k -af atempo=.\+ tmp-result.mp3$' лог.текст | wc -l) == 3 ]]
+set +x
 
 rm -rv testsuite/010-always-random-bpm/плеер-рут/portisculus-1 лог.текст
 
