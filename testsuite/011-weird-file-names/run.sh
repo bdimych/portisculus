bd1c3e9b4ca8611283adb8445440439ee101c719
bdimych <bdimych@faf0b38c-ac3b-0ead-faf0-b6b3d23b80b6>
2014-09-26 11:56:40 +0000

    testsuite/011-weird-file-names leading dashes in file names fixed

--- testsuite/011-weird-file-names/run.sh
+++ testsuite/011-weird-file-names/run.sh
@@ -2,5 +2,14 @@
 
 set -e -o pipefail
 
-ruby 2-fill-player.rb -dbf ./testsuite/011-weird-file-names/-.txt
+rm -rfv portisculus-1
+echo -n yyn | ruby 2-fill-player.rb -dbf ./testsuite/011-weird-file-names/-dbf.txt -prd .
+
+set -x
+[[ $(ls portisculus-1 | wc -l) == 6 ]]
+[[ $(cat portisculus-1/alreadyInPlayer.txt | wc -l) == 5 ]]
+
+rm -rv portisculus-1
+
+echo -OK -$0
 
